901ddfc7441205f72fdfad254c2c1a0534e8a69f
IngyN <ingyn@noreply.github.com>
2020-01-02 21:27:29 -0500

    add maddpg testing file

---
+++ test_maddpg.py
@@ -0,0 +1,16 @@
+import argparse
+import numpy as np
+import tensorflow as tf
+import time
+import pickle
+
+import maddpg.common.tf_util as U
+from maddpg.trainer.maddpg import MADDPGAgentTrainer
+import tensorflow.contrib.layers as layers
+
+from multiagent.environment import MultiAgentEnv
+import multiagent.scenarios as scenarios
+
+scenario = scenarios.load("simple" + ".py").Scenario()
+world = scenario.make_world()
+trainer = MADDPGAgentTrainer
