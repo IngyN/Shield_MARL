0f28cbbcfea5bf00f87a4382574a5347402beb81
IngyN <ingyn@noreply.github.com>
2020-01-09 16:11:19 -0500

    preliminary QL version

---
+++ QLearning.py
@@ -0,0 +1,59 @@
+import numpy as np
+import random
+
+
+class QLearning:
+    def __init__(self, map_size, nactions=5):
+        self.qvalues = np.zeros([map_size[0], map_size[1], nactions])
+        self.nactions = nactions
+
+    def action_selection(self, qval, epsilon=0.7):
+        # TODO : select action based on  previous Q-values or random
+        flag = random.random()
+        if flag >= epsilon:
+            # choose from qvalues
+            action = np.argmax(qval)
+        else:
+            # choose from random
+            action = random.randint(0, self.nactions - 1)
+
+        return action
+
+    def update(self, pos, obs, reward, action):
+        # TODO:update q-values
+        m_value = max(self.qvalues[obs[0]][obs[1]])
+        o_value = (1 - self.alpha) * self.qvalues[pos[0]][pos[1]][action]
+        self.qvalues[pos[0]][pos[1]][action] = self.alpha * (reward + self.discount * m_value) + o_value
+
+    def run(self, env, step_max=500, episode_max=1000):
+
+        env.reset()
+        alpha_index = 1
+        self.discount = 0.9
+        pos = env.pos
+        steps = np.zeros([episode_max])
+        stop = False
+
+        for e in range(episode_max):
+            for s in range(step_max):
+                self.alpha = alpha_index / (0.1 * s + 1)
+                action = self.action_selection(self.qvalues[pos[0]][pos[1]])
+                obs, rew, _, _ = env.step([action])
+                self.update(pos, obs[0], rew, action)
+                pos = obs[0]  # TODO check this - update position from observation
+                goal_flag = env.goal_flag
+
+                if goal_flag == 1:
+                    break
+
+            steps[e] = s
+            if e > 200:
+                stop = True
+                for stop_step in range(80):
+                    if steps[e] != steps[e - stop_step]:
+                        stop = False
+
+            if stop:
+                break
+
+        return self.qvalues
