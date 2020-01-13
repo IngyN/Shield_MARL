ea065d5e28884e9017fd0cb06e1df549c649906c
Ingy ElSayed-Aly <ingyn@github.com>
2020-01-13 14:40:10 -0500

    fixed and tested singleQL

--- QLearning.py
+++ QLearning.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import time
+from copy import deepcopy
 from gym_grid.envs import GridEnv
 
 
@@ -10,7 +11,6 @@ class QLearning:
         self.nactions = nactions
 
     def action_selection(self, qval, epsilon=0.7):
-        # TODO : select action based on  previous Q-values or random
         flag = random.random()
         if flag >= epsilon:
             # choose from qvalues
@@ -21,37 +21,49 @@ class QLearning:
 
         return action
 
-    def update(self, pos, obs, reward, action):
-        # TODO:update q-values
+    def update(self, pos, obs, reward, action, debug):
         m_value = max(self.qvalues[obs[0]][obs[1]])
         o_value = (1 - self.alpha) * self.qvalues[pos[0]][pos[1]][action]
+        # if debug:
+        #     print('old val: ', self.qvalues[pos[0]][pos[1]][action])
+        #     print('new_val:', self.alpha * (reward + self.discount * m_value) + o_value)
         self.qvalues[pos[0]][pos[1]][action] = self.alpha * (reward + self.discount * m_value) + o_value
 
-    def run(self, env, step_max=500, episode_max=1000, debug=False):
+    def run(self, env, step_max=500, episode_max=2000, discount=0.9, testing=False, debug=False):
 
-        env.reset()
         alpha_index = 1
-        self.discount = 0.9
-        pos = env.pos[0]
+        self.discount = discount
         # print(pos)
-        steps = np.zeros([episode_max])
+        steps = np.zeros([episode_max], dtype = int)
         stop = False
 
         for e in range(episode_max):
-            for s in range(step_max):
-                self.alpha = alpha_index / (0.1 * s + 1)
-                action = self.action_selection(self.qvalues[pos[0]][pos[1]])
-                obs, rew, _, _ = env.step([action])
-                self.update(pos, obs[0], rew, action)
+            env.reset()
+            pos = env.pos[0]
 
-                if debug:  # and s > step_max*0.6:
-                    time.sleep(2)
+            for s in range(step_max):
+
+                if debug:
                     env.render()
 
-                pos = obs[0]  # TODO check this - update position from observation
-                goal_flag = env.goal_flag
+                self.alpha = alpha_index / (0.1 * s + 0.5)
+                if not testing:
+                    action = self.action_selection(self.qvalues[pos[0]][pos[1]])
+                else:
+                    action = self.action_selection(self.qvalues[pos[0]][pos[1]], epsilon=0)
+                obs, rew, _, done = env.step([action])
+                if not testing:
+                    self.update(pos, obs[0], rew, action, True)
 
-                if goal_flag == 1:
+                if debug:  # and s > step_max*0.6:
+                    print('action:', action, ' -- done: ', done,  ' -- goal_flag:', env.goal_flag, ' --- rewards: ', rew, ' -- pos: ', pos, ' -- obs: ', obs)
+
+                pos = deepcopy(obs[0])  # TODO check this - update position from observation
+
+                if done:
+                    if debug:
+                        env.render()
+                    print('episode ', e, ' done')
                     break
 
             steps[e] = s
@@ -63,7 +75,7 @@ class QLearning:
 
             if stop:
                 break
-
+        print(steps+1)
         return self.qvalues
 
 
@@ -71,6 +83,9 @@ if __name__ == "__main__":
     env = GridEnv(agents=1)
     singleQL = QLearning([env.nrows, env.ncols])
     env.render()
-    input('next')
-    singleQL.run(env, step_max=400, episode_max=20, debug=True)
-    input('end')
+    # input('next')
+    qval = singleQL.run(env, step_max=400, episode_max=100, discount=0.9, debug=False)
+    # input('end')
+    print('----------------------------------------------------------------- \n end of training -----------------------------------------------------------------')
+    singleQL.run(env, step_max=400, episode_max=10, testing=True, debug=True)
+    env.final_render()
