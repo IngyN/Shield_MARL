d00bedd2a06fce586f6c5b51468f663a0ca9b655
IngyN <ingyn@noreply.github.com>
2020-02-13 16:44:40 -0500

    first version of the shield class !

--- Shield.py
+++ Shield.py
@@ -1,32 +1,71 @@
 import json
-
+import numpy as np
 
 class Shield:
 
     # Centralized shield version
-    def __init__(self, start, file='shields/collision_ISR_opt.shield'):
-        # TODO : asdd reading the file for shield here.
-        # find a more efficient way to load shield?
+    def __init__(self, nagents=2, start=[[7, 0], [6, 3]], file='shields/collision_ISR_opt.shield'):
         self.current_state = 0
+        self.nagents = nagents
 
         f = open(file)
         self.shield_json = json.load(f)
 
-        self._find_start_state(start)
+        found = self._find_start_state(start)
+        if found:
+            self.current_state = self.start_state
+            print('Start state = ', self.start_state)
+        else:
+            print('Error finding start state!!! ')
+            exit(1)
 
         f.close()
 
+    # search shield to find right start state based on agent start states
     def _find_start_state(self, start):
-        #TODO search shield to find right start state based on current state
-        self.start_state = 0
-        pass
+        found = False
 
-    def step(self, actions):
-        # TODO use info to determine next state
-        pass
+        for ind in range(len(self.shield_json)):
+            cur = self.shield_json[str(ind)]['State']
+            condition = np.zeros(self.nagents)
+
+            for i in range(self.nagents):
+                str_x = 'x' + str(i)
+                str_y = 'y' + str(i)
+                if start[i][0] == cur[str_x] and start[i][1] == cur[str_y]:
+                    condition[i] = 1
+
+            if np.count_nonzero(condition) == self.nagents:
+                found = True
+                self.start_state = ind  # state number.
+                break
+
+        return found
+
+    #  use actions and current shield state to determine if action is dangerous.
+    def step(self, act):
+
+        actions = np.zeros([self.nagents])
+        successors = np.array(self.shield_json[str(self.current_state)]['Successors'])
+
+        for s in successors:
+            cur = self.shield_json[str(s)]['State']
+            condition = np.zeros(self.nagents)
+            for i in range(self.nagents):
+                a_str = 'a' + str(i)
+                condition[i] = (cur[a_str] == actions[i])
+
+            if np.all(condition):  # found the correct successor
+                for i in range(self.nagents):
+                    s_str = 'shield' + str(i)
+                    actions[i] = cur[s_str]
+                    self.current_state = s
+                break
+
+        return actions
 
     def reset(self):
-        #TODO reset to start state.
+        # reset to start state.
         self.current_state = self.start_state
 
 
