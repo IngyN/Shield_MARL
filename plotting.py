c7b0b3b90d361f4705834b0f3c4ee1e0a7df98cc
IngyN <ingyesa@aucegypt.edu>
2020-02-25 15:46:04 -0500

    adjusted plotting.py
    Signed-off-by: IngyN <ingyn@noreply.github.com>

--- plotting.py
+++ plotting.py
@@ -5,56 +5,18 @@ from copy import deepcopy
 
 from mpl_toolkits.axes_grid1 import host_subplot
 
-
-def plot_and_save(data, agents, map='example', test=False, shielding=False, save=True, display=False):
-    plt.ioff()
-
-    fig = plt.figure(2)
-    plt.plot(np.arange(1, data['episodes'] + 1), data['steps'])
-    plt.title('Steps per episode')
-    if save:
-        if test:
-            fig.savefig('figures/cq_' + m + '_' + str(agents) + '_test_steps.png', bbox_inches='tight')
-        else:
-            fig.savefig('figures/cq_' + m + '_' + str(agents) + '_train_steps.png', bbox_inches='tight')
-
-    fig4 = plt.figure(4)
-    plt.plot(np.arange(1, episode_max + 1), acc[:, 0])
-    plt.plot(np.arange(1, episode_max + 1), acc[:, 1])
-    plt.title('Accumulated rewards per agent per episode')
-    if save:
-        fig4.savefig('figures/cq_' + m + '_' + str(agents) + '_train_acc.png', bbox_inches='tight')
-
-    fig5 = plt.figure(5)
-    plt.plot(np.arange(1, episode_max + 1), coll)
-    plt.title('Collisions')
-    if save:
-        fig5.savefig('figures/cq_' + m + '_' + str(agents) + '_train_coll.png', bbox_inches='tight')
-
-    if shielding:
-        fig6 = plt.figure(6)
-        plt.plot(np.arange(1, episode_max + 1), inter[0, :])
-        plt.plot(np.arange(1, episode_max + 1), inter[1, :])
-        plt.title('Shield Interference')
-        if save:
-            fig6.savefig('figures/cq_' + m + '_' + str(agents) + '_train_inter.png', bbox_inches='tight')
-
-    if display:
-        plt.show()
-
-
 def plot_v2(data, agents, map='example', test=False, shielding=False, save=True, display=False):
     plt.ioff()
     # Sent for figure
-    # font = {'size': 12}
-    # matplotlib.rc('font', **font)
+    font = {'size': 10}
+    matplotlib.rc('font', **font)
 
     # Setup figure and subplots
-    f1 = plt.figure(num=1, figsize=(12, 12))  # , dpi = 300)
+    f1 = plt.figure(num=1, figsize=(15, 15))  # , dpi = 300)
     if not test:
-        f1.suptitle("Training Results for " + str(agents) + ' agents on the ' + map + ' map', fontsize=14)
+        f1.suptitle("Training Results for " + str(agents) + ' agents on the ' + map + ' map', fontsize=12)
     else:
-        f1.suptitle("Test Results for " + str(agents) + ' agents on the ' + map + ' map', fontsize=14)
+        f1.suptitle("Test Results for " + str(agents) + ' agents on the ' + map + ' map', fontsize=12)
 
     ax01 = plt.subplot2grid((2, 2), (0, 0))
     ax02 = plt.subplot2grid((2, 2), (0, 1))
@@ -63,10 +25,10 @@ def plot_v2(data, agents, map='example', test=False, shielding=False, save=True,
     # plt.tight_layout()
 
     # Set titles of subplots
-    ax01.set_title('Steps per episode')
-    ax02.set_title('Accumulated rewards per agent per episode')
-    ax03.set_title('Collisions per Episode')
-    ax04.set_title('Interferences per Episode')
+    ax01.set_title('Steps per Episode', size=10)
+    ax02.set_title('Accumulated Rewards', size=10)
+    ax03.set_title('Collisions', size=10)
+    ax04.set_title('Interferences', size=10)
 
     # set y-limits
     ax01.set_ylim(0, 500)
@@ -120,23 +82,12 @@ def plot_v2(data, agents, map='example', test=False, shielding=False, save=True,
             inter_plots.append(p041)
             inter_labels.append(p041.get_label())
 
-    # set lagends
-
+    # set legends
     ax02.legend(acc_plots, acc_labels)
     if shielding:
         ax04.legend(inter_plots, inter_labels)
 
-    # # Plot Update
-    # figure(0)
-    # p011.set_data(t, yp1)
-    # p012.set_data(t, yp2)
-    #
-    # p021.set_data(t, yv1)
-    # p022.set_data(t, yv2)
-    #
-    # p031.set_data(t, yp1)
-    # p032.set_data(t, yv1)
-
+    plt.subplots_adjust(wspace=0.42, hspace=0.45)
     if save:
         if not test:
             f1.savefig('figures/cq_' + map + '_' + str(agents) + '_train.png', bbox_inches='tight')
