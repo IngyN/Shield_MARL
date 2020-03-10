16afb93ddc2e2acd86d3328583ced16e0156b220
IngyN <ingyesa@aucegypt.edu>
2020-03-10 15:08:45 -0400

    Update _find_start_state and constructor for grid shield
    Signed-off-by: IngyN <ingyn@noreply.github.com>

--- GridShield.py
+++ GridShield.py
@@ -5,7 +5,6 @@ import numpy as np
 class GridShield:
     # Composed shield version
     def __init__(self, env, nagents=2, start=np.array([[7, 0], [6, 3]]), file='example/example_2_agents_'):
-        self.current_state = 0
         self.nagents = nagents
         base = 'shields/grid_shields/'
         res = [3, 3]
@@ -19,6 +18,10 @@ class GridShield:
 
         self.nshields = int(np.max(smap))
         self.shield_json = []
+        self.current_state = np.zeros([self.nshields])
+        self.start_state = np.zeros([self.nshields])
+        self.agent_pos = np.ones([self.nagents, 2]) * -1
+        self.agent_shields = np.ones([self.nagents]) * -1
 
         # loading Shields.
         for sh in range(self.nshields):
@@ -26,32 +29,42 @@ class GridShield:
             self.shield_json.append(json.load(f))
             f.close()
 
-        # TODO update this.
-        found = self._find_start_state(start)
-        if found:
-            self.current_state = self.start_state
-            print('Start state = ', self.start_state)
-        else:
-            print('Error finding start state!!! ')
-            exit(1)
+        # check in which shields agents are starting
+        for i in range(self.nagents):
+            sh_ind = smap[start[i][0]][start[i][1]]
+            self.agent_shields[i] = sh_ind
+
+        # find the start state for each shield.
+        for sh in range(self.nshields):
+            ag = np.where(self.agent_shields == sh)[0]
+
+            found = self._find_start_state(start, sh, ag)
+            if found:
+                self.current_state = self.start_state
+            else:
+                print('State not found for shield : ', str(sh))
 
     # search shield to find right start state based on agent start states
-    def _find_start_state(self, start):
+    # i = index of shield, agents = indices of agents in shield i
+    def _find_start_state(self, start, i, agents):
         found = False
 
-        for ind in range(len(self.shield_json)):
-            cur = self.shield_json[str(ind)]['State']
-            condition = np.zeros(self.nagents)
+        for ind in range(len(self.shield_json[i])):
+            cur = self.shield_json[i][str(ind)]['State']
+            condition = np.zeros(agents)
 
-            for i in range(self.nagents):
+            for i in range(len(agents)):
                 str_x = 'x' + str(i)
                 str_y = 'y' + str(i)
                 if start[i][0] == cur[str_x] and start[i][1] == cur[str_y]:
                     condition[i] = 1
 
-            if np.count_nonzero(condition) == self.nagents:
+            if np.count_nonzero(condition) == agents:
                 found = True
-                self.start_state = ind  # state number.
+                self.start_state[i] = ind  # state number.
+                for a in agents:
+                    self.agent_pos[a][0] = np.where(agents == a)[0][0]
+                    self.agent_pos[a][1] = i
                 break
 
         return found
