fe7997625924102d19c2778af9d3e180f17a87a5
IngyN <ingyesa@aucegypt.edu>
2020-05-21 12:46:07 -0400

    hyper param comb
    Signed-off-by: IngyN <ingyesa@aucegypt.edu>

--- run_hyperparam_graphs.sh
+++ run_hyperparam_graphs.sh
@@ -1,8 +1,16 @@
-python smoothing.py -f logs/raw_CQ+shield_hyper_disc_6__2_23_15_17_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_hyper_disc_7__2_02_25_18_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_hyper_disc_8__2_05_37_18_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_hyper_disc_9__2_08_47_18_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_hyper_disc_10__2_11_49_18_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_10_1__2_03_21_20_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_10_2__2_06_26_20_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_10_3__2_09_32_20_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_10_4__2_12_44_20_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_15_1__2_15_43_20_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_15_2__2_18_49_20_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_15_3__2_21_58_20_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_15_4__2_01_17_21_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_5_1__2_15_06_19_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_5_2__2_18_00_19_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_5_3__2_21_05_19_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_5_4__2_00_23_20_May_2020.csv
+
 
 
 
