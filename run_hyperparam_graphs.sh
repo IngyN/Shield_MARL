e125f6e6acc23004729cd7ab1e9c00740c5b8776
IngyN <ingyesa@aucegypt.edu>
2020-05-22 17:40:42 -0400

    hyper param comb 2
    Signed-off-by: IngyN <ingyesa@aucegypt.edu>

--- run_hyperparam_graphs.sh
+++ run_hyperparam_graphs.sh
@@ -1,16 +1,9 @@
-python smoothing.py -f logs/raw_CQ+shield_hyper_comb_10_1__2_03_21_20_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_hyper_comb_10_2__2_06_26_20_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_hyper_comb_10_3__2_09_32_20_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_hyper_comb_10_4__2_12_44_20_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_hyper_comb_15_1__2_15_43_20_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_hyper_comb_15_2__2_18_49_20_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_hyper_comb_15_3__2_21_58_20_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_hyper_comb_15_4__2_01_17_21_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_hyper_comb_5_1__2_15_06_19_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_hyper_comb_5_2__2_18_00_19_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_hyper_comb_5_3__2_21_05_19_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_hyper_comb_5_4__2_00_23_20_May_2020.csv
-
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_5_3__2_19_15_21_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_5_25__2_16_07_21_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_5_35__2_22_34_21_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_15_06__2_01_31_22_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_15_08__2_04_26_22_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_hyper_comb_15_1__2_07_28_22_May_2020.csv
 
 
 
--- run_hyperparam_search.sh
+++ run_hyperparam_search.sh
@@ -1,24 +1,12 @@
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -h 5 -q 0.1 -e hyper_comb_5_1
-echo "Comb 5 0.1\n"
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -h 5 -q 0.2 -e hyper_comb_5_2
-echo "Comb 5 0.2\n"
+python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -h 5 -q 0.25 -e hyper_comb_5_25
+echo "Comb 5 0.25\n"
 python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -h 5 -q 0.3 -e hyper_comb_5_3
 echo "Comb 5 0.3\n"
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -h 5 -q 0.4 -e hyper_comb_5_4
-echo "Comb 5 0.4\n"
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -h 10 -q 0.1 -e hyper_comb_10_1
-echo "Comb 10 0.1\n"
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -h 10 -q 0.2 -e hyper_comb_10_2
-echo "Comb 10 0.2\n"
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -h 10 -q 0.3 -e hyper_comb_10_3
-echo "Comb 10 0.3\n"
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -h 10 -q 0.4 -e hyper_comb_10_4
-echo "Comb 10 0.4\n"
+python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -h 5 -q 0.35 -e hyper_comb_5_35
+echo "Comb 5 0.35\n"
+python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -h 15 -q 0.06 -e hyper_comb_15_06
+echo "Comb 15 0.06\n"
+python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -h 15 -q 0.08 -e hyper_comb_15_08
+echo "Comb 15 0.08\n"
 python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -h 15 -q 0.1 -e hyper_comb_15_1
 echo "Comb 15 0.1\n"
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -h 15 -q 0.2 -e hyper_comb_15_2
-echo "Comb 15 0.2\n"
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -h 15 -q 0.3 -e hyper_comb_15_3
-echo "Comb 15 0.3\n"
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -h 15 -q 0.4 -e hyper_comb_15_4
-echo "Comb 15 0.4\n"
