61ecc10062e283a21307ee937be4d25950cbdfaf
IngyN <ingyesa@aucegypt.edu>
2020-06-02 13:50:00 -0400

    hyper param ult
    Signed-off-by: IngyN <ingyesa@aucegypt.edu>

--- run_hyperparam_graphs.sh
+++ run_hyperparam_graphs.sh
@@ -1,7 +1,3 @@
-python smoothing.py -f logs/raw_CQ+shield_ultimate_1400__2_15_41_28_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_ultimate_1500__2_18_24_28_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_ultimate_1600__2_21_20_28_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_ultimate_1700__2_00_32_29_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_ultimate_1800__2_03_47_29_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_ultimate_1900__2_07_18_29_May_2020.csv
-python smoothing.py -f logs/raw_CQ+shield_ultimate_2000__2_10_59_29_May_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_ultimate_1200__2_15_52_01_Jun_2020.csv
+python smoothing.py -f logs/raw_CQ+shield_ultimate_1300__2_18_15_01_Jun_2020.csv
+
--- run_hyperparam_search.sh
+++ run_hyperparam_search.sh
@@ -1,14 +1,4 @@
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1400 -e ultimate_1400
-echo "utlimate 1400\n"
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1500 -e ultimate_1500
-echo "utlimate 1500\n"
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1600 -e ultimate_1600
-echo "utlimate 1600\n"
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1700 -e ultimate_1700
-echo "utlimate 1700\n"
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1800 -e ultimate_1800
-echo "utlimate 1800\n"
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1900 -e ultimate_1900
-echo "utlimate 1900\n"
-python run_exp_CQ.py -n 2 -p 1 -i 10 -t 2000 -e ultimate_2000
-echo "utlimate 2000\n"
+python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1200 -e ultimate_1200
+echo "utlimate 1200\n"
+python run_exp_CQ.py -n 2 -p 1 -i 10 -t 1300 -e ultimate_1300
+echo "utlimate 1300\n"
